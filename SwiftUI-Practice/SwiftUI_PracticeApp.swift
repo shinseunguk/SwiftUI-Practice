e0e356ca4d392ea8319876909df899b685762718
신승욱 <ukseung.dev@incross.com>
2024-04-05 15:00:12 +0900

    Initial Commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by ukseung.dev on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
