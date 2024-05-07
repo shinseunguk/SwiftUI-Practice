b5533b3a2b70fd0bfe360dadc85d21b18e559b3f
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-05-07 17:20:45 +0900

    hotfix: textfieldStyle 예제 구현 (#22)

--- SwiftUI-Practice/#18/ContentView18.swift
+++ SwiftUI-Practice/#18/ContentView18.swift
@@ -13,40 +13,44 @@ struct ContentView18: View {
     @State var pwValue: String = ""
     
     var body: some View {
-        VStack (alignment: .center){
+        VStack (spacing: 10){
             HStack {
                 TextField("ID", text: $idValue)
-                    .textFieldStyle(RoundTextFieldStyle())
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 Button(action: {
                     self.idValue = ""
                 }, label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .resizable()
-                        .foregroundColor(.gray)
-                        .frame(width: 20, height: 20)
-                        .opacity(idValue.count != 0 ? 1.0 : 0.0)
+                    
+                    if idValue.count > 0 {
+                        Image(systemName: "xmark.circle.fill")
+                            .resizable()
+                            .foregroundColor(.gray)
+                            .frame(width: 20, height: 20)
+                    }
                 })
             }
             
             HStack {
                 TextField("PW", text: $pwValue)
-                    .textFieldStyle(RoundTextFieldStyle())
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 Button(action: {
                     self.pwValue = ""
                 }, label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .resizable()
-                        .foregroundColor(.gray)
-                        .frame(width: 20, height: 20)
-                        .opacity(pwValue.count != 0 ? 1.0 : 0.0)
+                    if pwValue.count > 0 {
+                        Image(systemName: "xmark.circle.fill")
+                            .resizable()
+                            .foregroundColor(.gray)
+                            .frame(width: 20, height: 20)
+                    }
                 })
             }
             
             Text("비밀번호: \(pwValue)")
                 .padding(.top, 30)
         }
+        .padding(.horizontal, 50)
     }
 }
 
